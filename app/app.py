5bf6cffdc279b5bddff080a7665e9da89d70bc48
kskrm <laliga.bpl.crm7@gmail.com>
2021-12-16 22:29:03 +0900

    initiate app

--- app/app.py
+++ app/app.py
@@ -1,86 +1,25 @@
-from flask import Flask,render_template,request
-from models.models import OnegaiContent
-from models.database import db_session
-from datetime import datetime
-from flask import session,redirect,url_for
-from app import key
-from hashlib import sha256
+from flask import Flask,render_template,request,session,redirect
 
 app = Flask(__name__)
 
-app.secret_key = key.SECRET_KEY
+registerd = []
 
 @app.route("/")
-@app.route("/index")
 def index():
-    if "user_name" in session:
-        name = session["user_name"]
-        all_onegai = OnegaiContent.query.all()
-        return render_template("index.html", name=name, team=team, all_onegai=all_onegai)
-    else:
-        return redirect(url_for("top",status="logout"))
+    if request.method == "GET":
+        # display form to request birthday
+        return redirect("/register")
 
-@app.route("/index",methods=["post"])
-def post():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("index.html", name=name)
+
+@app.route("/register")
+def register():
     name = request.form["name"]
     team = ["Real Madrid","Manchester United","FC Barcelona","Manchester City"]
     all_onegai = OnegaiContent.query.all()
     return render_template("index.html", name=name, team=team, all_onegai=all_onegai)
-
-@app.route("/add",methods=["post"])
-def add():
-    title = request.form["title"]
-    body = request.form["body"]
-    content = OnegaiContent(title,body,datetime.now())
-    db_session.add(content)
-    db_session.commit()
-    return index()
-
-@app.route("/login",methods=["post"])
-def login():
-    user_name = request.form["user_name"]
-    user = User.query.filter_by(user_name=user_name).first()
-    if user:
-        password = request.form["password"]
-        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
-        if user.hashed_password == hashed_password:
-            session["user_name"] = user_name
-            return redirect(url_for("index"))
-        else:
-            return redirect(url_for("top",status="wrong_password"))
-    else:
-        return redirect(url_for("top",status="user_notfound"))
-
-@app.route("/registar",methods=["post"])
-def registar():
-    user_name = request.form["user_name"]
-    user = User.query.filter_by(user_name=user_name).first()
-    if user:
-        return redirect(url_for("newcomer",status="exist_user"))
-    else:
-        password = request.form["password"]
-        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
-        user = User(user_name, hashed_password)
-        db_session.add(user)
-        db_session.commit()
-        session["user_name"] = user_name
-        return redirect(url_for("index"))
-
-@app.route("/logout")
-def logout():
-    session.pop("user_name", None)
-    return redirect(url_for("top",status="logout"))
-
-@app.route("/top")
-def top():
-    status = request.args.get("status")
-    return render_template("top.html",status=status)
-
-
-@app.route("/newcomer")
-def newcomer():
-    status = request.args.get("status")
-    return render_template("newcomer.html",status=status)
 
 if __name__ == "__main__":
     app.run(debug=True)
